8592d14da8b5157d22c63ca062cbd92b0789a03d
ykmr0331 <ykmr0331@naver.com>
2023-05-23 21:30:24 +0900

    230523

--- JungsukJava/src/homePractice/J230523_AA_Honja_p121.java
+++ JungsukJava/src/homePractice/J230523_AA_Honja_p121.java
@@ -3,7 +3,15 @@ package homePractice;
 public class J230523_AA_Honja_p121 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int v2 = 1;
+		double v3 = 1.0;
+		
+		System.out.println(v2 ==v3); //true
+		
+		double v4 = 0.1;
+		float v5 = 0.1f;
+		System.out.println(v4 ==v5);
+		System.out.println((float)v4 ==v5);
 
 	}
 
