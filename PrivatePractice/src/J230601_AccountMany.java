3cde336d312b431a7e66e2082f1f75e49ef7b4f5
박재찬 <117189519+ykmr0331@users.noreply.github.com>
2023-06-01 19:01:02 +0900

    Add files via upload

---
+++ PrivatePractice/src/J230601_AccountMany.java
@@ -0,0 +1,10 @@
+
+public class J230601_AccountMany {
+	public J230601_Account acc0; // 초기화를 할 때 주소값이 들어가야돼
+	public J230601_Account acc1;
+	public J230601_Account acc2;
+	public J230601_Account acc3;
+	public J230601_Account acc4;
+	public int length;
+	
+}
