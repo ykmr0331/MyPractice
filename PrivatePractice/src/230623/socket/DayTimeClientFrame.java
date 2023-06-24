80913e215c2cae83c477a35d75975d1ab6ca8bac
박재찬 <117189519+ykmr0331@users.noreply.github.com>
2023-06-24 14:11:45 +0900

    Update DayTimeClientFrame.java

--- PrivatePractice/src/230623/socket/DayTimeClientFrame.java
+++ PrivatePractice/src/230623/socket/DayTimeClientFrame.java
@@ -28,11 +28,15 @@ public class DayTimeClientFrame extends JFrame {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					DayTimeClientFrame frame = new DayTimeClientFrame();
-					frame.setVisible(true);
+		EventQueue.invokeLater(new Runnable() {//invokeLater() 메서드는 이벤트 디스패치 스레드에서 실행할 작업을 예약합니다.
+														//  (사용자 인터페이스 이벤트를 처리하는 스레드)
+			
+			public void run() { //run메서드는 이벤트 디스패치 스레드에서 호출됌     
+				try {  
+					DayTimeClientFrame frame = new DayTimeClientFrame();//새 DayTimeClientFrame 개체를 생성
+					frame.setVisible(true); /*DayTimeClientFrame 개체의 가시성을 설정하는 메소드입니다. 
+											이 메소드의 인수는 boolean 값이며,
+											true이면 개체가 보이고, false이면 개체가 보이지 않습니다.*/
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -43,7 +47,7 @@ public class DayTimeClientFrame extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public DayTimeClientFrame() {
+	public DayTimeClientFrame() { // 클래스의 생성자
 		setTitle("DayTimeClient");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
@@ -55,25 +59,29 @@ public class DayTimeClientFrame extends JFrame {
 		
 		requestServerTimeButton = new JButton("Request Server Time");
 		requestServerTimeButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent e) {// 이 메서드는 버튼이 클릭될 때 호출됩니다.
 				/******************************************/
 				try {
 					/*
 					 * 1.Socket생성
 					 */
-					Socket socket=new Socket("192.168.15.31",8000);
+					Socket socket=new Socket("192.168.15.31",8000);/*서버에 연결하는 Socket 개체를 생성합니다.
+														     		서버의 IP 주소는 192.168.15.31이고, 포트 번호는 8000입니다.*/
+				
 					/*
 					 * 2.Socket으로부터 InputStream얻기
 					 *    - InputStream얻기
 					 *    - InputStream[byte stream] 을 Reader[character stream]로 변경하는
 					 * 			브릿지스트림(InputStreamReader) 을사용해서 변경
 					 */
-					InputStream in = socket.getInputStream();
-					InputStreamReader isr=new InputStreamReader(in);
-					BufferedReader br=new BufferedReader(isr);
-					String serverTime = br.readLine();
+					InputStream in = socket.getInputStream();//서버에서 입력 스트림을 얻습니다. 입력 스트림은 서버의 응답을 읽는 데 사용됩니다.
+					InputStreamReader isr=new InputStreamReader(in);/*코드는 입력 스트림을 문자 스트림으로 변환합니다. 
+																	이렇게 하면 서버의 응답을 문자열로 읽을 수 있습니다.*/
+					BufferedReader br=new BufferedReader(isr);//입력 스트림을 버퍼링합니다. 입력 스트림에서 데이터를 읽는 효율성을 향상시킴
+					String serverTime = br.readLine();// readLine():입력 스트림에서 한 줄의 문자열을 읽는 메소드입니다
 					//System.out.println(serverTime);
-					timeDispayLabel.setText(serverTime);
+					timeDispayLabel.setText(serverTime);/*timeDispayLabel 레이블에 serverTime 문자열을 표시합니다.
+														serverTime 문자열은 서버의 응답에서 읽은 현재 시간*/
 					br.close();
 					socket.close();
 				}catch (Exception e1) {
